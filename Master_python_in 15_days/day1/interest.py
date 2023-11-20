082beec1c44082629eea471f0f3772eca3e8b073
arvindjai <arvindjai22@gmail.com>
2023-11-20 18:50:13 +0530

    Modified interest.py file

--- Master_python_in 15_days/day1/interest.py
+++ Master_python_in 15_days/day1/interest.py
@@ -1,8 +1,8 @@
 # Calculate the compound interest for a given principal amount, interest rate, and time period
 principle = 10000
-rate = 10
+rate = 10/100
 n = 1
-t = 1
+t = 10
 expo = n*t
 amount = principle*(1 + (rate/n))**expo
 print(amount)
